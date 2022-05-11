3a510877a87199a0ffe71bbec96ff2ca943246db
Edward Rudd <urkle@outoforder.cc>
2022-05-11 15:27:31 -0400

    add in AR 6.1 and 7.0 support, also Ruby 3.1

--- lib/schema_plus/foreign_keys/active_record/connection_adapters/abstract/schema_creation.rb
+++ lib/schema_plus/foreign_keys/active_record/connection_adapters/abstract/schema_creation.rb
@@ -4,7 +4,18 @@ module SchemaPlus
   module Core
     module ActiveRecord
       module ConnectionAdapters
-        module AbstractAdapter
+        if Gem::Version.new(::ActiveRecord::VERSION::STRING) < Gem::Version.new('6.1')
+          module AbstractAdapter
+            module SchemaCreation
+
+              def visit_ForeignKeyDefinition(o)
+                # schema_plus_foreign_keys already implements a superior
+                # conversion of ForeignKeyDefinitions to SQL
+                o.to_sql
+              end
+            end
+          end
+        else
           module SchemaCreation
 
             def visit_ForeignKeyDefinition(o)
